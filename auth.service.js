a400ca07f88a2e08887a6413c164172047432c6e
Randy1burrell <randy1burrell@yahoo.com>
2017-08-11 00:27:07 -0500

    Inject browser window into authentication service and created saveToken function to be used to save web token to local storage

--- auth.service.js
+++ auth.service.js
@@ -6,4 +6,11 @@
     angular
         .module('CHANGE ME!!!!');
         .service('authentication');
+
+    authentication.inject = ['$window'];
+    function authentication($window) {
+        var saveToken = function(token) {
+            $window.localStorage['CHANGE ME!!!!'] = token;
+        };
+    }
 }) ();
