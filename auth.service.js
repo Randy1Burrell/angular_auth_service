a37df3b70829d883907468697e4611aacdc4f646
Randy1burrell <randy1burrell@yahoo.com>
2017-08-11 00:29:17 -0500

    Exposed saveToken function to public

--- auth.service.js
+++ auth.service.js
@@ -1,6 +1,14 @@
 /**
  * Author: Randy Burrell
  * Date: 2017/08/11
+ *
+ * Description: Angularjs service used for user authentication
+ * through JWT.
+ *
+ * Usage: Copy this file to your angular client services folder
+ * and change the module name below tothe module you would like
+ * to use it as. Also change the token name in saveToken, getToken
+ * and logout function to your prefered JSON Web Token
  */
 (function (){
     angular
@@ -11,6 +19,10 @@
     function authentication($window) {
         var saveToken = function(token) {
             $window.localStorage['CHANGE ME!!!!'] = token;
+        };
+
+        return {
+            saveToken : saveToken
         };
     }
 }) ();
